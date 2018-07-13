a23b2f97c24602b62999f95f796163706f0f231a
DonizeteVida <41035417+DonizeteVida@users.noreply.github.com>
2018-07-13 03:17:38 -0300

    MainActivity alterada 13.07 03:17

--- app/src/main/java/com/example/juniorsantos/barberapp/remote/MainActivity.java
+++ app/src/main/java/com/example/juniorsantos/barberapp/remote/MainActivity.java
@@ -18,11 +18,14 @@ import com.example.juniorsantos.barberapp.DAO.ConfiguracaoFirebase;
 import com.example.juniorsantos.barberapp.Entidades.Agendamento;
 import com.example.juniorsantos.barberapp.R;
 import com.example.juniorsantos.barberapp.core.DadosSingleton;
+import com.example.juniorsantos.barberapp.recyclerAdapter.AgendamentoCliente;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -34,13 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mbLogList;
     private DatabaseReference mDatabase;
-    private Agendamento agendamento;
-    private static String autenticacao;
-    List<Agendamento> agendament;
-
-
-
-
+    List<Agendamento> agendamentos;
 
 
     @Override
@@ -48,52 +45,87 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        autenticacao = FirebaseAuth.getInstance().getCurrentUser().getEmail(); //RETORNA O USUARIO LOGADO
+        agendamentos = new ArrayList<>();
 
-
-
-        mDatabase= FirebaseDatabase.getInstance().getReference().child("agendamento");
-
-
-
-
- //       mDatabase.orderByChild("emailCliente").equalTo(DadosSingleton.getInstance().getUser().getEmail());
-
-
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         mDatabase.keepSynced(true);
 
-        mbLogList=(RecyclerView)findViewById(R.id.myrecyclerview);
+        mbLogList = (RecyclerView) findViewById(R.id.myrecyclerview);
 
-        mbLogList.setHasFixedSize(true);
-        mbLogList.setLayoutManager(new LinearLayoutManager(this));
+        // mbLogList.setHasFixedSize(true);
+        //mbLogList.setLayoutManager(new LinearLayoutManager(this));
+
+        DatabaseReference aa = FirebaseDatabase.getInstance().getReference();
+
+        Query a = aa.child("agendamento");
+
+        Toast.makeText(this, DadosSingleton.getInstance().getUser().getEmail(), Toast.LENGTH_LONG).show();
+
+        a.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                agendamentos.clear();
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    agendamentos.add(dataSnapshot1.getValue(Agendamento.class));
+                }
+                atualizaRecycler();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void atualizaRecycler() {
+        mbLogList.setAdapter(new AgendamentoCliente(agendamentos, this, onClickAgendamentoCliente()));
+        mbLogList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        if (agendamentos.size() >= 1) {
+            mbLogList.scrollToPosition(mbLogList.getAdapter().getItemCount());
+        }
+
+
+    }
+
+    private AgendamentoCliente.onClickAgendamentoCliente onClickAgendamentoCliente() {
+        return new AgendamentoCliente.onClickAgendamentoCliente() {
+            @Override
+            public void onClick1(Agendamento a) {
+                mDatabase.child("agendamento").child(a.getIdAgendamento()).child("status").setValue("EM ATENDIMENTO");
+                Toast.makeText(MainActivity.this, "ATENDIMENTO INICIADO COM SUCESSO", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onClick2(Agendamento a) {
+                mDatabase.child("agendamento").child(a.getIdAgendamento()).child("status").setValue("FINALIZADO");
+                Toast.makeText(MainActivity.this, "ATENDIMENTO FINALIZADO COM SUCESSO", Toast.LENGTH_LONG).show();
+            }
+        };
     }
 
     @Override
     protected void onStart() {
-
         super.onStart();
-        Query query = mDatabase.orderByChild("idAgendamento");
+    /*
+        Toast.makeText(this, DadosSingleton.getInstance().getUser().getEmail(), Toast.LENGTH_LONG).show();
 
-        FirebaseRecyclerAdapter<Agendamento, BlogViewHolder>firebaseRecyclerAdapter=new FirebaseRecyclerAdapter<Agendamento, BlogViewHolder>
-                (Agendamento.class, R.layout.blog_row, BlogViewHolder.class, query) {
-
-
+        FirebaseRecyclerAdapter<Agendamento, BlogViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Agendamento, BlogViewHolder>
+                (Agendamento.class, R.layout.blog_row, BlogViewHolder.class, mDatabase) {
 
 
             @Override
+            protected void populateViewHolder(final BlogViewHolder viewHolder,
+                                              final Agendamento model, int position) {
 
-
-
-            protected void populateViewHolder(final BlogViewHolder viewHolder, final Agendamento model, int position) {
-
-
-                viewHolder.setData("DATA:      "+model.getDate());
-                viewHolder.setHora("HORARIO:   "+model.getHorario());
-                viewHolder.setNome("CLIENTE:   "+model.getNomeCliente());
+                viewHolder.setData("DATA:      " + model.getDate());
+                viewHolder.setHora("HORARIO:   " + model.getHorario());
+                viewHolder.setNome("CLIENTE:   " + model.getNomeCliente());
                 viewHolder.setImage(getApplicationContext(), model.getImageCliente());
                 viewHolder.setImgstatus(getApplicationContext(), model.getImgStatus());
 
-         /*       if (model.getStatus().equals("EM ATENDIMENTO")){
+                if (model.getStatus() == "EM ATENDIMENTO") {
 
                     viewHolder.btn.setEnabled(false);
                     viewHolder.btn2.setEnabled(true);
@@ -101,25 +133,10 @@ public class MainActivity extends AppCompatActivity {
 
                 }
 
-                if (model.getStatus().equals("FINALIZADO")){
+                if (model.getStatus() == "FINALIZADO") {
                     viewHolder.btn.setEnabled(false);
                     viewHolder.btn2.setEnabled(false);
                     mDatabase.child(model.getIdAgendamento()).child("imgStatus").setValue("https://png.pngtree.com/thumb_back/fw800/back_pic/04/22/64/495832ad8e872ee.jpg");
-                }
-
-                */
-                switch (model.getStatus()) {
-                    case "EM ATENDIMENTO":
-                        viewHolder.btn.setEnabled(false);
-                        viewHolder.btn2.setEnabled(true);
-                        mDatabase.child(model.getIdAgendamento()).child("imgStatus").setValue("https://media.istockphoto.com/photos/green-natural-background-picture-id649675846?k=6&m=649675846&s=612x612&w=0&h=mXTClbPjMIYE6OLNfLgCIR0GpJziAZM4oYlcJBw5EE8=");
-                        break;
-
-                    case "FINALIZADO":
-                        viewHolder.btn.setEnabled(false);
-                        viewHolder.btn2.setEnabled(false);
-                        mDatabase.child(model.getIdAgendamento()).child("imgStatus").setValue("https://png.pngtree.com/thumb_back/fw800/back_pic/04/22/64/495832ad8e872ee.jpg");
-                        break;
                 }
 
 
@@ -150,6 +167,7 @@ public class MainActivity extends AppCompatActivity {
         };
 
         mbLogList.setAdapter(firebaseRecyclerAdapter);
+        */
     }
 
 
@@ -157,7 +175,6 @@ public class MainActivity extends AppCompatActivity {
         View mView;
         public LinearLayout linearLayout;
         public Button btn, btn2;
-
 
 
         public BlogViewHolder(View itemView) {
@@ -194,7 +211,6 @@ public class MainActivity extends AppCompatActivity {
             CircleImageView post_Image = mView.findViewById(R.id.txistatus);
             Picasso.with(ctx).load(imgstatus).into(post_Image);
         }
-
 
     }
 }
